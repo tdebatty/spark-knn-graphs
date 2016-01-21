da22042c7b601ec698ab227223e7c04ea97f14f2
Thibault Debatty <thibault.debatty@gmail.com>
2016-01-21 14:04:41 +0100

    Style corrections

--- src/main/java/info/debatty/spark/knngraphs/builder/Online.java
+++ src/main/java/info/debatty/spark/knngraphs/builder/Online.java
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package info.debatty.spark.knngraphs.builder;
 
 import info.debatty.java.graphs.NeighborList;
@@ -47,97 +46,122 @@ import scala.Tuple2;
  * @param <T>
  */
 public class Online<T> {
+
     private final ApproximateSearch<T> searcher;
     private final int k;
     private final JavaSparkContext sc;
     private final SimilarityInterface<T> similarity;
-    
-    
+
+    /**
+     *
+     * @param k number of edges per node
+     * @param similarity similarity to use for computing edges
+     * @param sc spark context
+     * @param initial initial graph
+     */
     public Online(
-            int k,
-            SimilarityInterface<T> similarity,
-            JavaSparkContext sc,
-            JavaPairRDD<Node<T>, NeighborList> initial) {
-        
+            final int k,
+            final SimilarityInterface<T> similarity,
+            final JavaSparkContext sc,
+            final JavaPairRDD<Node<T>, NeighborList> initial) {
+
         this.similarity = similarity;
         this.k = k;
         this.sc = sc;
         searcher = new ApproximateSearch<T>(
                 initial,
-                5, 
+                5,
                 4,
                 similarity);
     }
-    
-    
-    public void addNode(Node<T> node) {
+
+    /**
+     *
+     * @param node to add to the graph
+     */
+    public final void addNode(final Node<T> node) {
         NeighborList neighborlist = searcher.search(
                 node,
                 k,
                 2000);
-        
+
         // Parallelize the pair node => neighborlist
-        LinkedList<Tuple2<Node<T>, NeighborList>> list = 
-                new LinkedList<Tuple2<Node<T>, NeighborList>>();
+        LinkedList<Tuple2<Node<T>, NeighborList>> list
+                = new LinkedList<Tuple2<Node<T>, NeighborList>>();
         list.add(new Tuple2<Node<T>, NeighborList>(node, neighborlist));
-        JavaPairRDD<Node<T>, NeighborList> new_graph_piece = sc.parallelizePairs(list);
-        
+        JavaPairRDD<Node<T>, NeighborList> new_graph_piece =
+                sc.parallelizePairs(list);
+
         // Partition the pair
         JavaPairRDD<Node<T>, NeighborList> partitioned_piece = partition(
                 new_graph_piece,
                 searcher.getMedoids(),
                 getCounts(),
                 searcher.getPartitioner());
-        
+
         // The new graph is the union of Java RDD's
         JavaPairRDD union = searcher.getGraph().union(partitioned_piece);
-        
-        
+
         // From now on, use the new graph...
         searcher.setGraph(union);
     }
-    
-    Long[] getCounts() {
-        List<Long> counts = searcher.getGraph().mapPartitions(new PartitionCountFunction(), true).collect();
-        
+
+    final Long[] getCounts() {
+        List<Long> counts = searcher.getGraph().mapPartitions(
+                new PartitionCountFunction(), true).collect();
+
         return counts.toArray(new Long[counts.size()]);
     }
 
-    JavaPairRDD<Node<T>, NeighborList> getGraph() {
+    final JavaPairRDD<Node<T>, NeighborList> getGraph() {
         return searcher.getGraph();
     }
-    
-        
+
     /**
-     * Perform a single iteration of partitioning, without recomputing new medoids
-     * @param piece
-     * @param medoids
-     * @param counts
-     * @param internal_partitioner
-     * @return 
+     * Perform a single iteration of partitioning, without recomputing new
+     * medoids.
+     *
+     * @param piece RDD containing subgraph to partition
+     * @param medoids medoids to use for partitioning
+     * @param counts current number of nodes per partition (used for bounding)
+     * @param internal_partitioner internal spark partitioner object
+     * @return a copy of the RDD, partitioned
      */
-    public JavaPairRDD<Node<T>, NeighborList> partition(
-            JavaPairRDD<Node<T>, NeighborList> piece,
-            List<Node<T>> medoids,
-            Long[] counts,
-            NodePartitioner internal_partitioner) {
-        
+    public final JavaPairRDD<Node<T>, NeighborList> partition(
+            final JavaPairRDD<Node<T>, NeighborList> piece,
+            final List<Node<T>> medoids,
+            final Long[] counts,
+            final NodePartitioner internal_partitioner) {
+
         // Assign each node to a partition id
-        JavaPairRDD<Node<T>, NeighborList> partitioned_graph = 
-                piece.mapPartitionsToPair(
+        JavaPairRDD<Node<T>, NeighborList> partitioned_graph
+                = piece.mapPartitionsToPair(
                         new AssignFunction<T>(medoids, counts, similarity),
                         true);
 
         // Partition
         partitioned_graph = partitioned_graph.partitionBy(internal_partitioner);
-        
+
         return partitioned_graph;
     }
 
-    private static class PartitionCountFunction<U> 
-        implements FlatMapFunction<Iterator<Tuple2<Node<U>, NeighborList>>, Long> {
-            
-        public Iterable<Long> call(Iterator<Tuple2<Node<U>, NeighborList>> iterator) throws Exception {
+    /**
+     *
+     * @param <U>
+     */
+    private static class PartitionCountFunction<U>
+            implements FlatMapFunction
+            <Iterator<Tuple2<Node<U>, NeighborList>>, Long> {
+
+        /**
+         *
+         * @param iterator
+         * @return
+         * @throws Exception
+         */
+        public Iterable<Long> call(
+                final Iterator<Tuple2<Node<U>, NeighborList>> iterator)
+                throws Exception {
             long count = 0;
             while (iterator.hasNext()) {
                 iterator.next();
@@ -150,24 +174,32 @@ public class Online<T> {
         }
     }
 }
-    
 
-
+/**
+ *
+ * @author Thibault Debatty
+ * @param <U>
+ */
 class AssignFunction<U>
-    implements PairFlatMapFunction<Iterator<Tuple2<Node<U>, NeighborList>>, Node<U>, NeighborList> {
+        implements PairFlatMapFunction
+        <Iterator<Tuple2<Node<U>, NeighborList>>, Node<U>, NeighborList> {
+
     private final List<Node<U>> medoids;
     private final Long[] counts;
-    public final SimilarityInterface<U> similarity;
+    private final SimilarityInterface<U> similarity;
 
-    public AssignFunction(List<Node<U>> medoids, Long[] counts, SimilarityInterface<U> similarity) {
+    public AssignFunction(
+            final List<Node<U>> medoids,
+            final Long[] counts,
+            final SimilarityInterface<U> similarity) {
         this.medoids = medoids;
         this.counts = counts;
         this.similarity = similarity;
     }
 
-    public Iterable<Tuple2<Node<U>, NeighborList>> 
-        call(Iterator<Tuple2<Node<U>, NeighborList>> iterator) 
-                throws Exception {
+    public Iterable<Tuple2<Node<U>, NeighborList>>
+            call(final Iterator<Tuple2<Node<U>, NeighborList>> iterator)
+            throws Exception {
 
         // Total number of elements
         long n = sum(counts) + 1;
@@ -175,10 +207,10 @@ class AssignFunction<U>
         int partitions = medoids.size();
         int partition_constraint = (int) (imbalance * n / partitions);
 
-        // fetch all tuples in this partition 
+        // fetch all tuples in this partition
         // to compute the partition_constraint
-        ArrayList<Tuple2<Node<U>, NeighborList>> tuples = 
-                new ArrayList<Tuple2<Node<U>, NeighborList>>();
+        ArrayList<Tuple2<Node<U>, NeighborList>> tuples
+                = new ArrayList<Tuple2<Node<U>, NeighborList>>();
 
         while (iterator.hasNext()) {
             Tuple2<Node<U>, NeighborList> tuple = iterator.next();
@@ -194,49 +226,52 @@ class AssignFunction<U>
                         tuple._1.value);
             }
 
-            // 2. value to maximize = similarity * (1 - cluster_size / capacity_constraint)
+            // 2. value to maximize :
+            // similarity * (1 - cluster_size / capacity_constraint)
             for (int center_id = 0; center_id < partitions; center_id++) {
-                values[center_id] = similarities[center_id] *
-                        (1 - counts[center_id] / partition_constraint);
+                values[center_id] = similarities[center_id]
+                        * (1 - counts[center_id] / partition_constraint);
             }
 
             // 3. choose partition that minimizes compute value
             int partition = argmax(values);
             counts[partition]++;
-            tuple._1.setAttribute(BalancedKMedoidsPartitioner.PARTITION_KEY, partition);
+            tuple._1.setAttribute(
+                    BalancedKMedoidsPartitioner.PARTITION_KEY,
+                    partition);
         }
 
         return tuples;
     }
-        
-    private static long sum(Long[] values) {
-            long agg = 0;
-            for (long value : values) {
-                agg += value;
-            }
-            return agg;
+
+    private static long sum(final Long[] values) {
+        long agg = 0;
+        for (long value : values) {
+            agg += value;
         }
-        
-    private static int argmax(double[] values) {
+        return agg;
+    }
+
+    private static int argmax(final double[] values) {
         double max_value = -1.0 * Double.MAX_VALUE;
         ArrayList<Integer> ties = new ArrayList<Integer>();
-        
+
         for (int i = 0; i < values.length; i++) {
             if (values[i] > max_value) {
                 max_value = values[i];
                 ties = new ArrayList<Integer>();
                 ties.add(i);
-                
-            } else if(values[i] == max_value) {
+
+            } else if (values[i] == max_value) {
                 // add a tie
                 ties.add(i);
             }
         }
-        
+
         if (ties.size() == 1) {
             return ties.get(0);
         }
-        
+
         Random rand = new Random();
         return ties.get(rand.nextInt(ties.size()));
     }
--- src/test/java/info/debatty/spark/knngraphs/builder/OnlineTest.java
+++ src/test/java/info/debatty/spark/knngraphs/builder/OnlineTest.java
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package info.debatty.spark.knngraphs.builder;
 
 import info.debatty.java.graphs.NeighborList;
@@ -45,45 +44,32 @@ import scala.Tuple2;
  * @author Thibault Debatty
  */
 public class OnlineTest extends TestCase implements Serializable {
-    
-    public OnlineTest(String testName) {
-        super(testName);
-    }
-    
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
 
     /**
      * Test of addNode method, of class Online.
+     *
      * @throws java.lang.Exception
      */
     public void testAddNode() throws Exception {
         System.out.println("addNode");
-        
+
         Logger.getLogger("org").setLevel(Level.WARN);
         Logger.getLogger("akka").setLevel(Level.WARN);
-        
+
         // Number of nodes in the initial graph
         int n = 500;
-        
+
         // Number of nodes to add to the graph
         int n_add = 20;
         int partitions = 4;
-        
+
         SimilarityInterface<Double> similarity = new SimilarityInterface<Double>() {
-            
+
             public double similarity(Double value1, Double value2) {
                 return 1.0 / (1 + Math.abs(value1 - value2));
             }
         };
-        
+
         System.out.println("Create some random nodes");
         Random rand = new Random();
         List<Node<Double>> data = new ArrayList<Node<Double>>();
@@ -92,41 +78,38 @@ public class OnlineTest extends TestCase implements Serializable {
             data.add(new Node<Double>(String.valueOf(data.size()), 150.0 + 100.0 * rand.nextGaussian()));
             data.add(new Node<Double>(String.valueOf(data.size()), 300.0 + 100.0 * rand.nextGaussian()));
         }
-        
 
         // Configure spark instance
         SparkConf conf = new SparkConf();
         conf.setAppName("SparkTest");
         conf.setIfMissing("spark.master", "local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        
+
         // Parallelize the dataset in Spark
         JavaRDD<Node<Double>> nodes = sc.parallelize(data, partitions);
-        
+
         Brute brute = new Brute();
         brute.setK(10);
         brute.setSimilarity(similarity);
-        
+
         System.out.println("Compute the graph and force execution");
         JavaPairRDD<Node<Double>, NeighborList> graph = brute.computeGraph(nodes);
         System.out.println(graph.first());
-        
+
         System.out.println("Prepare the graph for online processing");
         Online<Double> online_graph = new Online<Double>(10, similarity, sc, graph);
-        
-        
+
         System.out.println("Add some nodes...");
         for (int i = 0; i < n_add; i++) {
             Node<Double> new_node = new Node<Double>("", 400.0 * rand.nextDouble());
             System.out.println(new_node);
-            online_graph.addNode(new_node);   
+            online_graph.addNode(new_node);
         }
         List<Tuple2<Node<Double>, NeighborList>> local_graph = online_graph.getGraph().collect();
         sc.close();
-        
+
         assertEquals(data.size() + n_add, local_graph.size());
         assertEquals(online_graph.getGraph().partitions().size(), partitions);
-        
+
     }
-    
 }
