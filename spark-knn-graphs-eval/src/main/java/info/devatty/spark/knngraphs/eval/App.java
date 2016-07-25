47e9cca119ef8e8e6811c3747ea7b0ea31f2ce53
Thibault Debatty <thibault.debatty@gmail.com>
2016-07-25 11:23:10 +0200

    Created eval project

---
+++ spark-knn-graphs-eval/src/main/java/info/devatty/spark/knngraphs/eval/App.java
@@ -0,0 +1,13 @@
+package info.devatty.spark.knngraphs.eval;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
