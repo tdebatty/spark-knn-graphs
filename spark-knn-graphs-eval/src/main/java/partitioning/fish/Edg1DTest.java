97b524296890cd380215a8411b6ebfa11113e0a5
Thibault Debatty <thibault.debatty@gmail.com>
2017-07-24 10:14:13 +0200

    Add partitioning test with fish dataset

---
+++ spark-knn-graphs-eval/src/main/java/partitioning/fish/Edg1DTest.java
@@ -0,0 +1,71 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 tibo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package partitioning.fish;
+
+import info.debatty.java.datasets.fish.TimeSerie;
+import info.debatty.java.graphs.NeighborList;
+import info.debatty.java.graphs.Node;
+import info.debatty.jinu.TestInterface;
+import info.debatty.spark.knngraphs.Edge1DPartitioner;
+import info.debatty.spark.knngraphs.JaBeJa;
+import info.debatty.spark.knngraphs.Partitioning;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+/**
+ *
+ * @author tibo
+ */
+public class Edg1DTest implements TestInterface {
+
+    public static String dataset_path;
+
+    @Override
+    public final double[] run(final double budget) throws Exception {
+
+        SparkConf conf = new SparkConf();
+        conf.setAppName("Spark graph partitioning with SPAM");
+        conf.setIfMissing("spark.master", "local[*]");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<Tuple2<Node<TimeSerie>, NeighborList>> tuples =
+                sc.objectFile(dataset_path);
+        JavaPairRDD<Node<TimeSerie>, NeighborList> graph =
+                JavaPairRDD.fromJavaRDD(tuples);
+
+        Edge1DPartitioner<TimeSerie> partitioner =
+                new Edge1DPartitioner<TimeSerie>(16);
+        Partitioning<TimeSerie> partition = partitioner.partition(graph);
+        double[] result = new double[] {
+            JaBeJa.countCrossEdges(partition.graph, 16),
+            JaBeJa.computeBalance(partition.graph, 16)
+        };
+        sc.close();
+
+        return result;
+    }
+}
