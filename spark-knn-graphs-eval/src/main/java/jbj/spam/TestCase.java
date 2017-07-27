a33687683849dea9f88037b554fe7b728f77855b
Thibault Debatty <thibault.debatty@gmail.com>
2017-07-27 21:08:54 +0200

    Added Ja-Be-Ja convergence test with spam dataset

---
+++ spark-knn-graphs-eval/src/main/java/jbj/spam/TestCase.java
@@ -0,0 +1,77 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 tibo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jbj.spam;
+
+import info.debatty.jinu.Case;
+import java.util.Arrays;
+import java.util.List;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author tibo
+ */
+public class TestCase {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception if anything goes wrong
+     */
+    public static void main(final String[] args) throws Exception {
+
+        OptionParser parser = new OptionParser("i:r:d:t:");
+        OptionSet options = parser.parse(args);
+
+        JaBeJaTest.dataset_path = (String) options.valueOf("d");
+
+        int iterations = Integer.valueOf((String) options.valueOf("i"));
+
+        List<String> time_list = (List<String>) options.valuesOf("t");
+        double[] times = new double[time_list.size()];
+        for (int i = 0; i < times.length; i++) {
+            times[i] = Double.valueOf(time_list.get(i));
+        }
+
+        // Reduce Spark output logs
+        Logger.getLogger("org").setLevel(Level.WARN);
+        Logger.getLogger("akka").setLevel(Level.WARN);
+
+        Case test = new Case();
+        test.setDescription(TestCase.class.getName() + " : "
+                + String.join(" ", Arrays.asList(args)));
+        test.setIterations(iterations);
+        test.setParallelism(1);
+        test.commitToGit(false);
+        test.setBaseDir((String) options.valueOf("r"));
+        test.setParamValues(times);
+
+        test.addTest(JaBeJaTest.class);
+
+        test.run();
+
+    }
+}
